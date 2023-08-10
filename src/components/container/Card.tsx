bad6868bd3bb6fc7c9b80e8320ce24858b547256
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-08-10 19:26:43 +0300

    update: Card removed h-max
    Having h-max added nothing to the component.

--- src/components/container/Card.tsx
+++ src/components/container/Card.tsx
@@ -3,12 +3,11 @@
 import { HTMLAttributes } from 'react';
 import Surface from '@/components/container/Surface';
 
-export default function Card({children, className, ...restProps}:  & HTMLAttributes<HTMLDivElement>) {
+export default function Card({children, className = '', ...restProps}: HTMLAttributes<HTMLDivElement>) {
   return (
     <Surface className={`
       rounded
       p-5 py-8 lg:p-10
-      h-max
       shadow-dp6 shadow-shadow/50
       ${className}
     `} {...restProps}>{children}</Surface>
