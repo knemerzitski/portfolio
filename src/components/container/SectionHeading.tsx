ab80adb51d6ace6fcfbd4829ace4b900254b1678
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-08-10 19:41:36 +0300

    update: adjusted heading pad for mobile

--- src/components/container/SectionHeading.tsx
+++ src/components/container/SectionHeading.tsx
@@ -4,7 +4,7 @@ export default function SectionHeading({ children }: { children: ReactNode }) {
   return (
     <div className="
       bg-primary-800
-      py-14 mb-20
+      py-10 xs:py-14 mb-20
       shadow-dp12 shadow-shadow/25
     ">
       <h2 className="
