161c51c0f04d6f4896282217a6c3b293dd01f379
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-08-17 18:04:05 +0300

    update: create dataLayer before consent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ import { ModalsContextProvider } from '@/contexts/ModalsContext';
 import Anchor from '@/components/nav/Anchor';
 import { Body } from '@/components/container/Body';
 import { HashContextProvider } from '@/contexts/HashContext';
-import { ConsentedGoogleTagManager } from '@/libs/gtm';
+import { GoogleTagManagerScript } from '@/libs/gtm';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -39,7 +39,7 @@ export default function RootLayout({ children, }: { children: ReactNode, }) {
           h-screen
       `}>
         <CookieConsentContextProvider>
-          <ConsentedGoogleTagManager />
+          <GoogleTagManagerScript />
 
           <ModalsContextProvider>
 
--- src/libs/gtm.tsx
+++ src/libs/gtm.tsx
@@ -2,8 +2,15 @@
 
 import Script from "next/script";
 import { NEXT_PUBLIC_GTM_ID } from "./env";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useCookieConsentContext } from "@/contexts/CookieConsentContext";
+
+type WindowWithDataLayer = Window & {
+  dataLayer?: any[]
+}
+
+declare const window: WindowWithDataLayer;
+
 
 const GTM_ID = NEXT_PUBLIC_GTM_ID;
 
@@ -11,9 +18,22 @@ export function isEnabled() {
   return GTM_ID != null;
 }
 
-export function ConsentedGoogleTagManager() {
+export function GoogleTagManagerScript() {
   const cookieConsentContext = useCookieConsentContext();
   const [hasAnalyticsConsent, setHasAnalyticsConsent] = useState(false);
+  const pushGtmStartOnceRef = useRef(true);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    window.dataLayer = window.dataLayer || [];
+
+    if(!pushGtmStartOnceRef.current) return;
+    pushGtmStartOnceRef.current = false;
+
+    console.log('dataLayer pushed gtm.start');
+    window.dataLayer.push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+  }, []);
 
   useEffect(() => {
     setHasAnalyticsConsent(cookieConsentContext?.getConsent()?.analytics ?? false);
@@ -29,21 +49,19 @@ function ScriptInjector() {
       <>
         <Script id="google-tag-manager" strategy="afterInteractive" dangerouslySetInnerHTML={{
           __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            (function(w,d,s,l,i){var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
           `
         }} />
-        {/* This code never happens due to consent requiring javascript */}
-        <noscript>
-          <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{
-            display: 'none',
-            visibility: 'hidden'
-          }}>
-          </iframe>
-        </noscript>
+        {/* This code never runs due to consent requiring javascript. Must use cookies for consent for this to work */}
+        <noscript dangerouslySetInnerHTML={{
+          __html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe><>
+        `
+        }} />
       </>
     );
   }
