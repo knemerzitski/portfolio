5a7e9e99c7cd84fb592ef05c67c8732ba1dbcd86
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-08-20 00:19:16 +0300

    fix: more strict referrer policy

--- next.config.js
+++ next.config.js
@@ -27,7 +27,7 @@ const nextConfig = {
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin'
+            value: 'same-origin'
           },
           {
             key: 'X-Frame-Options',
