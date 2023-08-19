c0c2f8a55388c7ef5235145ffaea027766f2ce6d
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-08-19 13:20:09 +0300

    add: sitemap project pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,18 @@
 import { APP_ORIGIN } from '@/libs/env'
 import { MetadataRoute } from 'next'
- 
+import { projects } from '@/app/portfolio';
+
+const lastModified = new Date();
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: APP_ORIGIN,
-      lastModified: new Date(),
+      lastModified,
     },
+    ...projects.map((project) => ({
+      url: `${APP_ORIGIN}/project/${project.slug}`,
+      lastModified
+    })),
   ]
 };
